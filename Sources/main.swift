8e4cfd0491f98f3554e00b20820f015ba7eb3524
giggino <luigi.borriello2@studio.unibo.it>
2026-06-10 20:35:37 +0200

    Open new iTerm2 window instead of tab, force floating mode and focus

--- Sources/main.swift
+++ Sources/main.swift
@@ -28,11 +28,25 @@ func getFinderPath() -> String? {
 }
 
 func openInITerm2(path: String) {
-    let task = Process()
-    task.launchPath = "/usr/bin/open"
-    task.arguments = ["-b", "com.googlecode.iterm2", path]
-    task.launch()
-    task.waitUntilExit()
+    let escapedPath = path.replacingOccurrences(of: "\\", with: "\\\\")
+        .replacingOccurrences(of: "\"", with: "\\\"")
+    let script = "tell application \"iTerm2\"\n" +
+        "set newWindow to create window with default profile\n" +
+        "tell newWindow\n" +
+        "set fullscreen to false\n" +
+        "set bounds to {100, 100, 900, 600}\n" +
+        "select\n" +
+        "end tell\n" +
+        "tell current session of newWindow\n" +
+        "write text \"cd \\\"\(escapedPath)\\\"\"\n" +
+        "end tell\n" +
+        "end tell"
+    let appleScript = NSAppleScript(source: script)
+    var error: NSDictionary?
+    appleScript?.executeAndReturnError(&error)
+    if let errorInfo = error {
+        NSLog("AppleScript error: \(errorInfo)")
+    }
 }
 
 guard let finderPath = getFinderPath() else {
